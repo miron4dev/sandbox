ef4c3e336c211fa5ab31cadad1bdbf14fe9f5551
miron4dev <miron4dev@gmail.com>
2019-06-08 18:11:08 +0300

    jni project has been created

---
+++ jni/src/main/java/com/miron4dev/sandbox/jni/App.java
@@ -0,0 +1,18 @@
+package com.miron4dev.sandbox.jni;
+
+public class App {
+
+    static {
+        System.loadLibrary("bridge");
+    }
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Argument is missing");
+        }
+        int n = Integer.parseInt(args[0]);
+
+        Fibonacci fibonacci = new Fibonacci();
+        System.out.println(fibonacci.fib(n));
+    }
+}
---
+++ jni/src/main/java/com/miron4dev/sandbox/jni/Fibonacci.java
@@ -0,0 +1,6 @@
+package com.miron4dev.sandbox.jni;
+
+public class Fibonacci {
+
+    public native int fib(int n);
+}
