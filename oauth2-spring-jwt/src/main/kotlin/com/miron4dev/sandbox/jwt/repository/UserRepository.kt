8c153a90897fc46eefe9fe24e63704bd217df638
miron4dev <miron4dev@gmail.com>
2019-06-21 22:53:14 +0300

    oauth2-spring-jwt project has been moved to a separate repository

--- oauth2-spring-jwt/src/main/kotlin/com/miron4dev/sandbox/jwt/repository/UserRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.miron4dev.sandbox.jwt.repository
-
-import com.miron4dev.sandbox.jwt.model.User
-import org.springframework.data.repository.PagingAndSortingRepository
-
-interface UserRepository : PagingAndSortingRepository<User, Long> {
-
-    fun existsByAccountId(accountId: Long): Boolean
-}
